c0996efb98d1724770e014b2fadc75f4d0d0c822
Munna-Mahmud <learnermunna@gmail.com>
2021-09-17 17:52:53 +0600

    practis splice

---
+++ es6/splice.js
@@ -0,0 +1,6 @@
+let names = ['vinod', 'bahadur', 'thapa']
+// const newname  =names.splice(names.length,0,'thapatechnical') // add a elements et the end of this array
+// console.log(names)
+
+const  newname  =names.splice(1,1,'BAHADUR')
+console.log(names) // insend of bahadur it will be now BHAHADUR 
--- es6/templetelLiteral.js
+++ es6/templetelLiteral.js
@@ -6,3 +6,7 @@ console.log(`My name is ${name} iD: ${iD}`)
 const p = 23;
 const vara =232;
 console.log(`${vara + iD}`)
+
+
+
+
